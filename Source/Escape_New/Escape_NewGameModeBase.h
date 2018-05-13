e3100a92e1589a75121afb6628f9712df67da5d2
David Moreland <dcmoreland@hotmail.com>
2018-05-13 13:15:49 -0500

    Add files via upload
    Syncing Remote

---
+++ Source/Escape_New/Escape_NewGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Escape_NewGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPE_NEW_API AEscape_NewGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
